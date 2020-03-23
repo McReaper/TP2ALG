e286ebd25ae5e2cba06c3aeb069303632eac2c0f
mcreaper <mcreaper@hotmail.fr>
2020-03-23 15:17:25 +0100

    ajout de file.h dans a234.c

--- file.c
+++ file.c
@@ -8,7 +8,7 @@ pfile_t creer_file ()
   pfile_t nouvelleFile = malloc(sizeof(file_t));
   nouvelleFile->tete = 0;
   nouvelleFile->queue = 0;
-  for (int i = 0; i < MAX_FILE_SIZE; i ++) nouvelleFile->Tab[i] = -1;
+  for (int i = 0; i < MAX_FILE_SIZE; i ++) nouvelleFile->Tab[i] = NULL;
   return nouvelleFile;
 }
 
@@ -22,7 +22,7 @@ int detruire_file (pfile_t f)
 
 int file_vide (pfile_t f)
 {
-  return (f->queue == f->tete && f->Tab[f->tete] == -1 ? 1 : 0);
+  return (f->queue == f->tete && f->Tab[f->tete] == NULL ? 1 : 0);
 }
 
 int file_pleine (pfile_t f)
@@ -30,51 +30,26 @@ int file_pleine (pfile_t f)
   if (f->queue != f->tete) return 0;
   int plein = 1;
   for (int i = 0; i < MAX_FILE_SIZE && plein; i++) {
-    if (f->Tab[i] == -1) plein = 0;
+    if (f->Tab[i] == NULL) plein = 0;
   }
   return plein;
 }
 
-int defiler (pfile_t f)
+Arbre234 defiler (pfile_t f)
   {
     if(file_vide(f))
-      return -1;
-    int elem = f->Tab[f->tete];
-    f->Tab[f->tete] = -1;
+      return NULL;
+    Arbre234 elem = f->Tab[f->tete];
+    f->Tab[f->tete] = NULL;
     f->tete = (f->tete >= MAX_FILE_SIZE-1 ? 0 : f->tete + 1);
     return elem;
   }
 
-int enfiler (pfile_t f, int p)
+int enfiler (pfile_t f, Arbre234 p)
 {
   if (file_pleine(f))
-    return -1;
+    return 1;
   f->Tab[f->queue]=p;
   f->queue = (f->queue >= MAX_FILE_SIZE-1 ? 0: f->queue + 1);
   return 0 ;
-}
-
-void afficher_file(pfile_t f){
-
-  if ( file_vide(f)){
-    printf("<-- | file vide | <--\n");
-  } else {
-    //file non vide
-    printf("<-- |");
-
-    int currentPos = f->tete;
-    if (currentPos == f->queue){
-      printf(" %d |", f->Tab[currentPos]);
-      currentPos++;
-    }
-    while (currentPos != f->queue){
-      printf(" %d |", f->Tab[currentPos]);
-      if (currentPos == MAX_FILE_SIZE-1){
-        currentPos = 0; //file circulaire !
-      } else {
-        currentPos++;
-      }
-    }
-    printf(" <--\n");
-  }
 }
--- file.h
+++ file.h
@@ -4,7 +4,7 @@
 
 typedef struct {
   int tete, queue ;
-  int Tab [MAX_FILE_SIZE] ;
+  Arbre234 Tab [MAX_FILE_SIZE] ;
 } file_t, *pfile_t ;
 
 typedef file_t file;
@@ -36,23 +36,19 @@ Donnée : une pile f.
 int file_pleine (pfile_t f) ;
 
 /*
-Résultat: renvoie la val supprimé. NULL si f est vide
+Résultat: renvoie le noeud supprimé. NULL si f est vide
 Donnée : une pile f.
 Pré-condition: f est non vide.
 Post-condition: supprime l'élément à la queue de f.
 Effets de bord: f est modifiée
 */
-int defiler (pfile_t f)  ;
+Arbre234 defiler (pfile_t f)  ;
 
 /*
 Résultat: 1 si Erreur (file déjà pleine); 0 sinon
 Données : une pile f.
-Post-condition: ajoute la val p a la tete de f.
+Post-condition: ajoute le noeud p a la tete de f.
 Effets de bord: f est modifiée
 */
-int enfiler (pfile_t f, int p) ;
+int enfiler (pfile_t f, Arbre234 p) ;
 
-/*
-Résultat : Affichage de la file
-*/
-void afficher_file(pfile_t f);
