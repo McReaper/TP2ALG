c27810ce1340b5b8ef58f9571ad84a1c051d1640
cdlk <popoto38697@gmail.com>
2020-03-23 10:50:35 +0100

    CleMin et CleMax

--- a234.c
+++ a234.c
@@ -10,7 +10,7 @@
 int hauteur (Arbre234 a)
 {
   int h0, h1, h2, h3 ;
-  
+
   if (a == NULL)
     return 0 ;
 
@@ -23,33 +23,43 @@ int hauteur (Arbre234 a)
   h3 = hauteur (a->fils [3]) ;
 
   return 1 + max (max (h0,h1),max (h2,h3)) ;
-} 
+}
 
 int NombreCles (Arbre234 a)
 {
   /*
      Retourne le nombre de cles presentes dans l'arbre a
   */
-  
+
   return 0 ;
+}
+
+int GetIndexMax(Arbre234 a){
+  //Renvoie l'index du plus grand fils dans un noeud => si 0 alors type noeud = 0
+  return (a->t > 0? a->t-1 , a->t);
 }
 
 int CleMax (Arbre234 a)
 {
-  /*
-     plus grande cle de l'arbre a
-  */
-  
-  return 0 ;
+  Arbre234 current = a;
+  Arbre234 prec = current;
+  while(GetIndexMax(current) != 0){
+    prec = current;
+    current = current->fils[GetIndexMax(current)];
+  }
+  return prec->cle[GetCleMax(prec)];
 }
 
 int CleMin (Arbre234 a)
 {
-  /*
-     Retourne plus petite cle de l'arbre a
-  */
+  Arbre234 current = a;
+  Arbre234 prec = current;
+  while(current->t != 0){
+    prec = current;
+    current = current->fils[0];
+  }
+  return prec->cle[0];
 
-  return 0 ;
 }
 
 Arbre234 RechercherCle (Arbre234 a, int cle)
@@ -64,9 +74,16 @@ Arbre234 RechercherCle (Arbre234 a, int cle)
 
 void AnalyseStructureArbre (Arbre234 a, int *feuilles, int *noeud2, int *noeud3, int *noeud4)
 {
-  /* 
+  /*
      calculer le nombre de feuilles, de 2-noeuds, 3-noeuds,et 4-noeuds
   */
+  if(a->t != 0){
+    int i = 0;
+    while(i != GetIndexMax(a)){
+      AnalyseStructureArbre(a->fils[i], feuilles, noeud2, noeud3, noeud4);
+      i++;
+    }
+  }
 }
 
 Arbre234 noeud_max (Arbre234 a)
@@ -74,7 +91,7 @@ Arbre234 noeud_max (Arbre234 a)
   /*
     Retourne le noeud avec la somme maximale des cles internes
   */
-  
+
   return NULL ;
 }
 
@@ -91,16 +108,16 @@ void Afficher_Cles_Largeur (Arbre234 a)
 
 void Affichage_Cles_Triees_Recursive (Arbre234 a)
 {
-  /* 
+  /*
      Afficher les cles en ordre croissant
      Cette fonction sera recursive
   */
-     
+
 }
 
 void Affichage_Cles_Triees_NonRecursive (Arbre234 a)
 {
-    /* 
+    /*
      Afficher les cles en ordre croissant
      Cette fonction ne sera pas recursive
      Utiliser une pile
@@ -134,7 +151,7 @@ int main (int argc, char **argv)
   a = lire_arbre (argv [1]) ;
 
   printf ("==== Afficher arbre ====\n") ;
-  
+
   afficher_arbre (a, 0) ;
 
 }
